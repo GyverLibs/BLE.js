43ee6bd428d24e5cb0100273d1deef4ac4f19746
AlexGyver <alexgyvershow@ya.ru>
2026-05-05 01:13:11 +0300

    upd

--- test/script.js
+++ test/script.js
@@ -1,18 +1,25 @@
 import BLEJS from "https://gyverlibs.github.io/BLE.js/ble.min.js";
-// import BLEJS from "../ble";
+// import BLEJS from "../ble2light.js";
 
 let i = 0;
-let ble = new BLEJS({ auto_open: true, eol: '' });
+let ble = new BLEJS({ auto_open: true });
 
 // control
 select_b.onclick = () => ble.select();
 open_b.onclick = () => ble.open();
 close_b.onclick = () => ble.close();
-send_b.onclick = () => ble.sendText('Hello ' + i++);
+send_b.onclick = async () => {
+    ble.sendBin(new TextEncoder().encode('Hello ' + i++));
+}
+
+async function send(hex) {
+    const data = new Uint8Array(hex.trim().split(/\s+/).map(x => parseInt(x, 16)));
+    await ble.sendBin(data);
+}
 
 // read
-// ble.onbin = b => console.log(b);
-ble.ontext = t => console.log(t);
+ble.onbin = b => console.log(new TextDecoder().decode(b));
+// ble.ontext = t => console.log(t);
 
 // state
 // ble.onopen = () => {
